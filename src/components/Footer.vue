5517a5338cc401543f704e1a234abb0e5b421a28
YuleBest <yule-best@outlook.com>
2025-08-16 03:38:35 +0800

    refactor(Footer): 移除未使用的社交媒体图标代码

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-divider :thickness="2"></v-divider>
   <v-footer class="text-center d-flex flex-column ga-2 py-4">
-    <div class="d-flex ga-3">
+    <!-- <div class="d-flex ga-3">
       <v-btn
         v-for="icon in icons"
         :key="icon"
@@ -9,14 +9,14 @@
         density="comfortable"
         variant="text"
       ></v-btn>
-    </div>
+    </div> -->
 
     <div>{{ new Date().getFullYear() }} — <strong>于乐Yule</strong></div>
   </v-footer>
 </template>
 
 <script setup>
-const icons = ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"];
+// const icons = ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"];
 </script>
 
 <style scoped></style>
