fb27e4a6eb89b5d85669195ca73440c6799f867c
YuleBest <yule-best@outlook.com>
2025-08-18 00:38:00 +0800

    feat: 添加不蒜子统计功能
    在页脚添加访问次数统计，并引入不蒜子统计脚本

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,13 @@
 <template>
   <v-divider :thickness="2"></v-divider>
   <v-footer class="text-center d-flex flex-column ga-2 py-4">
-    <!-- <div class="d-flex ga-3">
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        :icon="icon"
-        density="comfortable"
-        variant="text"
-      ></v-btn>
-    </div> -->
-
+    <div>本站已被访问过 <span id="busuanzi_site_pv"></span> 次</div>
     <div>{{ new Date().getFullYear() }} — <strong>于乐Yule</strong></div>
   </v-footer>
 </template>
 
 <script setup>
-// const icons = ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"];
+// 这里不需要特别的逻辑
 </script>
 
 <style scoped></style>
